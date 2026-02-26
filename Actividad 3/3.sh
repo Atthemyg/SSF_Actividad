f69a49daba3f84fb1afddcbec56fc7315231857f
Atteneri Bravo Barroso <atteneribravobarroso@gmail.com>
2026-02-26 00:01:24 +0000

    Hago el apartado 3

---
+++ Actividad 3/3.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+#CONTROL DE ERRORES
+set -euo pipefail
+
+ALTURA_CM=$1
+PESO=$2
+
+IMC=$(( PESO / (ALTURA_CM * ALTURA_CM) ))
+
+echo "IMC: $IMC"
+
+if (( $IMC < 18 )); then
+  echo "Bajo peso"
+elif (( $IMC > 18 && $IMC < 25 )); then
+  echo "Peso normal"
+elif (( $IMC > 25 && $IMC < 30 )); then
+  echo "Sobrepeso"
+else
+  echo "Obesidad"
+fi

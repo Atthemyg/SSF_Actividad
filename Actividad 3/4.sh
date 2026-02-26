b6ea181ac779c3f120acfc44b38a2e6f6b78c95c
Atteneri Bravo Barroso <atteneribravobarroso@gmail.com>
2026-02-26 00:23:50 +0000

    Hago el apartado 4

---
+++ Actividad 3/4.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+
+#CONTROL DE ERRORES
+set -euo pipefail
+
+while true
+do
+  read -p "Introduce contraseña: " PASS1
+  echo
+  read -p "Confirma contraseña: " PASS2
+  echo
+
+  if [ "$PASS1" = "$PASS2" ]; then
+    echo "OK - Contraseña guardada"
+    break
+  else
+    echo "ERROR - No coinciden. Inténtalo de nuevo."
+  fi
+done

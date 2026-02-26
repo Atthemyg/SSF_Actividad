7c26641d212c02fd6b916855f5ac4201fddcec0e
Atteneri Bravo Barroso <atteneribravobarroso@gmail.com>
2026-02-26 00:32:41 +0000

    Hago el apartado 5

---
+++ Actividad 3/5.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/bash
+
+#CONTROL DE ERRORES
+set -euo pipefail
+
+echo "1. Espacio libre en disco (%)"
+echo "2. Espacio libre (tamaño)"
+echo "3. Usuario actual y nombre de la máquina"
+echo "4. Número de usuarios en la máquina"
+echo "5. Espacio usado en tu carpeta"
+
+read OPCION
+
+if [ "$OPCION" = "1" ]; then
+  df -h
+elif [ "$OPCION" = "2" ]; then
+  df -h
+elif [ "$OPCION" = "3" ]; then
+  echo $USER
+  hostname
+elif [ "$OPCION" = "4" ]; then
+  wc -l /etc/passwd
+elif [ "$OPCION" = "5" ]; then
+  du -sh $HOME
+else
+  echo "Opción no válida"
+fi
